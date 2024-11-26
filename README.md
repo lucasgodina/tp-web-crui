b63a25bb61cfe3c5ab3fe075a0360067c198f79b
Lucas Godina <lucasgodina@gmail.com>
2024-11-26 19:51:01 -0300

    Add initial project files with HTML, CSS, and JavaScript setup

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Trabajo Práctico - Diseño Web
+
 Este es el proyecto final para la materia Diseño Web de la carrera Tecnicatura en Desarrollo de Software del CRUI.
 
-Integrantes:
+## Integrantes:
+
 - Galeano, Jorgelina
 - Godina, Lucas
 - Maez, Jazmin
---
+++ js/main.js
@@ -0,0 +1,14 @@
+const api = 'https://potterapi-fedeperin.vercel.app/es';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		// Validar formulario
+
+		// Obtener datos de la API
+
+		// Mostrar resultados en la tabla HTML
+	});
+});
